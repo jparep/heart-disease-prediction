77a852a9d7ce915bd6341fdc34c40f68f098eb4b
jparep <parepson@gmail.com>
2024-08-30 00:03:37 -0600

    load data

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def load_data(file_path):
     return pd.read_csv(file_path)
 
 def main():
-    curdir = 'data/raw'
+    curdir = 'data/raw/'
+    merge_files(curdir)
     
     file_path = 'data/raw/heart_2020_cleaned.csv'
     df = load_data(file_path)
