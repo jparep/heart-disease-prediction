283a34bbf5ea37dd14850edccf7e14e26664dc0c
jparep <parepson@gmail.com>
2024-08-29 23:48:35 -0600

    Merge files method added

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import os
 
+# Merge files
+def merge_files(curdir):
+    for dirname, _, filenames in os.walk(curdir):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
 # Load Data
 def load_data(file_path):
     return pd.read_csv(file_path)
 
 def main():
+    curdir = 'data/raw'
+    
     file_path = 'data/raw/heart_2020_cleaned.csv'
     df = load_data(file_path)
+    print(df.head())
 
 if __name__=='__main__':
     main()
