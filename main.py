7896b3a3d4d9027175b717e66861a69f6f3165c5
jparep <parepson@gmail.com>
2024-09-01 23:44:14 -0600

    Remove unnecessary fearures

--- main.py
+++ main.py
@@ -5,18 +5,31 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
+import logging
 
-# Load Data
+
+
 def load_data(file_path):
+    """Load Data from csv file"""
     return pd.read_csv(file_path)
+
+def convert_cat_to_num(df):
+    pass
+    
 
 def main():
     
     # Load data
     file_path = 'data/processed/merged_heart_data.csv'
     df = load_data(file_path)
+    
+    # Drop unncessary columns
+    df = df.drop('Unnamed: 0', axis=1)
+    
+    # Drop na from target variable
     df = df.dropna(subset=['HeartDisease'])
     print(f"Total NA for target varible = {df['HeartDisease'].isna().sum()}")
 
+    print(df.columns)
 if __name__=='__main__':
     main()
