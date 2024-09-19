00833435868b6ff677a1da1e62cbf411abc0cb9e
jparep <parepson@gmail.com>
2024-09-19 14:26:39 -0600

    Import loging and typing

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, classification_report
+from typing import Tuple, Dict, Any
+import logging
+import os
+import joblib
